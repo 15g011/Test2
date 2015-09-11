313dd5187d7e67bad74cb164580cff40ecbf41f7
unknown <x15g011@PC505N011.chiba-fjb.ac.jp>
2015-09-11 11:51:18 +0900

    変更

--- src/test/kana.java
+++ src/test/kana.java
@@ -4,6 +4,9 @@ public class kana {
 
 	public static void main(String[] args) {
 		System.out.println("ああわあ");
+		System.out.println("ああわあ");
+		System.out.println("ああわあ");
+		System.out.println("ああわあ");
 
 	}
 
